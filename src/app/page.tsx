e97e2fd2670ba62fb35f18395c2ff608c6ef3439
khalidkarimkqr <khalid13878@hotmail.com>
2025-03-24 09:49:17 -0700

    installed some more react packages for styling our components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,5 @@ import {env} from '@/env';
 import {} from "ai/rsc";
 
 export default function Home() {
-  const example = env.OPEN_AI_API_KEY;
-  console.log({example});
-  return <main> {example} </main>;
+  return <main> </main>;
 }
