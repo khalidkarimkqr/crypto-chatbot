ff6636e477868f6477e0458cf16878a334143e0e
khalidkarimkqr <khalid13878@hotmail.com>
2025-03-25 08:12:03 -0700

    Created use-enter-submit for a user to press enter in the form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,12 @@
 
 import { ChatList } from "@/components/chat-list";
 import ChatScrollAnchor from "@/components/chat-scroll-anchor";
+import { useEnterSubmit } from "@/lib/use-enter-submit";
 import { useForm } from "react-hook-form";
 
 export default function Home() {
   const form = useForm();
-
+  const { formRef } = useEnterSubmit();
   const onSubmit = () => {};
   return (
     <main>
