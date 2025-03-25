52b3eeb9e6e943f8bc23b49e2e8ffe08171d3fee
khalidkarimkqr <khalid13878@hotmail.com>
2025-03-25 07:27:41 -0700

    Created a use-at-bottom tsx file for handling scroll till the end

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { env } from "@/env";
 import {} from "ai/rsc";
+import { ChatList } from "@/components/chat-list";
 
 export default function Home() {
   return (
     <main>
       <div className="pb-[200px] pt-4 md:pt-10">
-        <h1>Hello?</h1>
+        <ChatList messages={[]} />
       </div>
     </main>
   );
---
+++ src/components/chat-scroll-anchor.tsx
@@ -0,0 +1,3 @@
+"use client";
+
+import { useAtBottom } from "@/lib/use-at-bottom";
