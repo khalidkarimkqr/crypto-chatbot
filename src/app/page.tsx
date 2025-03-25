c16b730c3463bcfe32bf63d62e60db7961f7b62c
khalidkarimkqr <khalid13878@hotmail.com>
2025-03-25 07:50:01 -0700

    updated the page.tsx file to include a form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,22 @@
-import {} from "ai/rsc";
+"use client";
+
 import { ChatList } from "@/components/chat-list";
 import ChatScrollAnchor from "@/components/chat-scroll-anchor";
+import { useForm } from "react-hook-form";
 
 export default function Home() {
+  const form = useForm();
+
+  const onSubmit = () => {};
   return (
     <main>
       <div className="pb-[200px] pt-4 md:pt-10">
         <ChatList messages={[]} />
         <ChatScrollAnchor />
       </div>
-      <h1>This is the anchored content</h1>
+      <div className="">
+        <form ref={formRef} onSubmit={form.handleSubmit(onSubmit)}></form>
+      </div>
     </main>
   );
 }
