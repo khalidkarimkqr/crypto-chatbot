263b0848e13aa1515eec5cd9105a2502ea348c49
khalidkarimkqr <khalid13878@hotmail.com>
2025-03-25 08:24:14 -0700

    made the form look more better using textareaautosize and added attributes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { ChatList } from "@/components/chat-list";
 import ChatScrollAnchor from "@/components/chat-scroll-anchor";
 import { useEnterSubmit } from "@/lib/use-enter-submit";
 import { useForm } from "react-hook-form";
+import TextareaAutosize from "react-textarea-autosize";
 
 export default function Home() {
   const form = useForm();
@@ -18,7 +19,17 @@ export default function Home() {
       <div className="">
         <form ref={formRef} onSubmit={form.handleSubmit(onSubmit)}>
           <div className="relative flex flex-col w-full overflow-hidden max-h-60 grow bg-background sm:rounded-md sm:border">
-            <h1>Hello this is a chat box</h1>
+            <TextareaAutosize
+              tabIndex={0}
+              onKeyDown={onKeyDown}
+              placeholder="Send a message"
+              className="min-h-[60px] w-full resize-none bg-transparent pl-4 pr-16 py-[1.3rem] focus-within:outline-none sm:text-sm"
+              autoFocus
+              spellCheck={false}
+              autoComplete="off"
+              autoCorrect="off"
+              rows={1}
+            />
           </div>
         </form>
       </div>
