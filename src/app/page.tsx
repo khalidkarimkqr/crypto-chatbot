ea1747a93c73ea5fe1dc97bad6759f6b3c5a7a33
khalidkarimkqr <khalid13878@hotmail.com>
2025-03-25 07:16:12 -0700

    Created a chat-list tsx component file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
-import {env} from '@/env';
+import { env } from "@/env";
 import {} from "ai/rsc";
 
 export default function Home() {
-  return <main> </main>;
+  return (
+    <main>
+      <div className="pb-[200px] pt-4 md:pt-10">
+        <h1>Hello?</h1>
+      </div>
+    </main>
+  );
 }
